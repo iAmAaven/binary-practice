364ee50037bc9a64f28f769518d889ea14983855
Aapo Harjunpää <aapo.harjunpaa@tuni.fi>
2025-02-05 02:08:55 +0200

    Added new game mode (major update)
    + Added Bullet Binary
    + Added customizable color themes
    + Added high score system
    + Added saving and loading through PlayerPrefs

--- BinaryPractice/Assets/Scripts/BinaryMenu.cs
+++ BinaryPractice/Assets/Scripts/BinaryMenu.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BinaryMenu : MonoBehaviour
 {
-    public GameObject mainMenu, settingsMenu, gameModeMenu;
+    public GameObject mainMenu, settingsMenu, gameModeMenu, gameOverMenu;
     public BinaryPractice binaryPractice;
+    public Color defaultTextColor, defaultBackgroundColor;
 
+
+    void Start()
+    {
+        Light2D globalLight = FindFirstObjectByType<Light2D>();
+
+        globalLight.color = new Color(
+            PlayerPrefs.GetFloat("TextColor_R", defaultTextColor.r),
+            PlayerPrefs.GetFloat("TextColor_G", defaultTextColor.g),
+            PlayerPrefs.GetFloat("TextColor_B", defaultTextColor.b),
+            PlayerPrefs.GetFloat("TextColor_A", defaultTextColor.a));
+
+        Camera cam = Camera.main;
+
+        cam.backgroundColor = new Color(
+            PlayerPrefs.GetFloat("Background_R", defaultBackgroundColor.r),
+            PlayerPrefs.GetFloat("Background_G", defaultBackgroundColor.g),
+            PlayerPrefs.GetFloat("Background_B", defaultBackgroundColor.b),
+            PlayerPrefs.GetFloat("Background_A", defaultBackgroundColor.a));
+    }
 
     void Update()
     {
@@ -20,6 +41,11 @@ public class BinaryMenu : MonoBehaviour
             else if (gameModeMenu.activeSelf)
             {
                 gameModeMenu.SetActive(false);
+                mainMenu.SetActive(true);
+            }
+            else if (gameOverMenu.activeSelf)
+            {
+                gameOverMenu.SetActive(false);
                 mainMenu.SetActive(true);
             }
             else if (mainMenu.activeSelf)
--- BinaryPractice/Assets/Scripts/ColorButton.cs
+++ BinaryPractice/Assets/Scripts/ColorButton.cs
@@ -11,10 +11,30 @@ public class ColorButton : MonoBehaviour
     {
         globalLight = FindFirstObjectByType<Light2D>();
         globalLight.color = GetComponent<Image>().color;
+
+        float color_R = globalLight.color.r;
+        float color_G = globalLight.color.g;
+        float color_B = globalLight.color.b;
+        float color_A = globalLight.color.a;
+
+        PlayerPrefs.SetFloat("TextColor_R", color_R);
+        PlayerPrefs.SetFloat("TextColor_G", color_G);
+        PlayerPrefs.SetFloat("TextColor_B", color_B);
+        PlayerPrefs.SetFloat("TextColor_A", color_A);
     }
     public void ChooseBackgroundColor()
     {
         cam = Camera.main;
         cam.backgroundColor = GetComponent<Image>().color;
+
+        float color_R = cam.backgroundColor.r;
+        float color_G = cam.backgroundColor.g;
+        float color_B = cam.backgroundColor.b;
+        float color_A = cam.backgroundColor.a;
+
+        PlayerPrefs.SetFloat("Background_R", color_R);
+        PlayerPrefs.SetFloat("Background_G", color_G);
+        PlayerPrefs.SetFloat("Background_B", color_B);
+        PlayerPrefs.SetFloat("Background_A", color_A);
     }
 }
